5e998cb32dc0fd392d74579eb92647c87809f894
Hafizur Rahman <bibagimon@gmail.com>
2018-07-18 23:38:35 +0900

    Sigma points initialized

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,34 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  if (estimations.size() == 0) {
+    cout << "[ERROR] [CalculateRMSE] Estimations vector is empty" << endl;
+    return rmse;
+  }
+
+  if (ground_truth.size() == 0) {
+    cout << "[ERROR] [CalculateRMSE] Ground-truth vector is empty" << endl;
+    return rmse;
+  }
+
+  unsigned int n = estimations.size();
+  if (n != ground_truth.size()) {
+    cout << "[ERROR] [CalculateRMSE] The ground-truth and estimations vectors must have the same size." << endl;
+    return rmse;
+  }
+
+  for (unsigned int i=0; i < n; i++) {
+    VectorXd diff = estimations[i] - ground_truth[i];
+    diff = diff.array() * diff.array();
+
+    rmse += diff;
+  }
+  
+  rmse = rmse / n;
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.3;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
@@ -54,6 +54,17 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  // Weights of sigma points
+  weights_ = VectorXd(2 * n_aug_ + 1);
+
+  // State dimension
+  n_x_ = 5;
+
+  // Augmented state dimension
+  n_aug_ = 7;
+
+  // Sigma point spreading parameter
+  lambda_ = 3 - n_x_;
 }
 
 UKF::~UKF() {}
@@ -69,6 +80,46 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if (
+    (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_) ||
+    (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_)
+  ) {
+    if (!is_initialized_) {
+      time_us_ = meas_package.timestamp_;
+
+      P_ << 0.15,    0, 0, 0, 0,
+               0, 0.15, 0, 0, 0,
+               0,    0, 1, 0, 0,
+               0,    0, 0, 1, 0,
+               0,    0, 0, 0, 1;
+
+      if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_) {
+       float ro =  meas_package.raw_measurements_(0);
+       float phi =  meas_package.raw_measurements_(1);
+       float ro_dot =  meas_package.raw_measurements_(2);
+
+       x_(0) = ro * cos(phi);
+       x_(1) = ro * sin(phi);
+      } else if (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_) {
+       x_(0) = meas_package.raw_measurements_(0);
+       x_(1) = meas_package.raw_measurements_(1);
+      }
+
+      is_initialized_ = true;
+      return;
+    }
+
+    // Predict first
+    double delta_t = (meas_package.timestamp_ - time_us_)/1000000.0;
+    Prediction(delta_t);
+
+    // Update second
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      UpdateRadar(meas_package);
+    } else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+      UpdateLidar(meas_package);
+    }
+  }  
 }
 
 /**
@@ -83,6 +134,17 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  //create sigma point matrix
+  MatrixXd Xsig_ = MatrixXd(n_x_, 2 * n_x_ + 1);
+
+  //calculate square root of P_
+  MatrixXd A_ = P_.llt().matrixL();
+
+  Xsig_.col(0) = x_;  
+  for (int i = 0; i < n_x_; i++) {
+      Xsig_.col(i + 1) = x_ + sqrt(lambda_ + n_x_)*A_.col(i);
+      Xsig_.col(i + 1 + n_x_) = x_ - sqrt(lambda_ + n_x_)*A_.col(i);
+  }
 }
 
 /**
